b1cd57b632f0ac8cee897680bc7ec96d654eaa21
msj <1498218391@qq.com>
2026-04-01 16:35:51 +0800

    feat: 配置前端代理

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // 只要你在 Vue 里请求以 /api 开头的地址
+      // 比如 axios.get('/api/users/')
+      // Vite 就会自动帮你转发到 Django 的 8000 端口
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        // 如果你的 Django 路由没有 /api 前缀，可以取消下面这行的注释
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
